cf6eee7c73e3b1ad290522f3722a6a0c7c2a928b
robot8a <cie.hochoa@gmail.com>
2024-10-23 19:13:44 +0200

    Download project statistics, move file names to variable in download script

--- 02_download_hotosm.sh
+++ 02_download_hotosm.sh
@@ -17,17 +17,43 @@ echo "***************************"
 PAYLOAD="{\"as_file\": false, \"abbreviated\": true}"
 current_project=1
 for project_id in $project_ids; do
+    filename="data/project_${project_id}.json"
     while : ; do
-        if [ ! -f "data/project_${project_id}.json" ]; then
-            echo "Downloading data/project_${project_id}.json ... STATUS(${current_project} of ${num_project_ids})"
-            curl -s -X GET -H "Accept-Language: en;accept: application/json" -d "${PAYLOAD}" "${API_ENDPOINT}projects/${project_id}/" > "data/project_${project_id}.json"
+        if [ ! -f "${filename}" ]; then
+            echo "Downloading ${filename} ... STATUS(${current_project} of ${num_project_ids})"
+            curl -s -X GET -H "Accept-Language: en;accept: application/json" -d "${PAYLOAD}" "${API_ENDPOINT}projects/${project_id}/" > "${filename}"
             if [ $? -eq 0 ]; then
                 break
             fi
             echo "Failed to download project ${project_id}. Retrying in 3 seconds..."
             sleep 3
         else
-            echo "File data/project_${project_id}.json already exists. Skipping download."
+            echo "File ${filename} already exists. Skipping download."
+            break
+        fi
+    done
+    current_project=$((current_project + 1))
+done
+
+# Download project data
+echo "****************************************"
+echo "DOWNLOADING PROJECT STATISTICS DATA..."
+echo "****************************************"
+PAYLOAD="{}"
+current_project=1
+for project_id in $project_ids; do
+    filename="data/project_${project_id}_statistics.json"
+    while : ; do
+        if [ ! -f "${filename}" ]; then
+            echo "Downloading ${filename} ... STATUS(${current_project} of ${num_project_ids})"
+            curl -s -X GET -H "Accept-Language: en;accept: application/json" -d "${PAYLOAD}" "${API_ENDPOINT}projects/${project_id}/statistics/" > "${filename}"
+            if [ $? -eq 0 ]; then
+                break
+            fi
+            echo "Failed to download project ${project_id}. Retrying in 3 seconds..."
+            sleep 3
+        else
+            echo "File ${filename} already exists. Skipping download."
             break
         fi
     done
@@ -42,15 +68,16 @@ API_ENDPOINT="https://production-raw-data-api.s3.amazonaws.com/TM/"
 PAYLOAD="{}"
 current_project=1
 for project_id in $project_ids; do
+    filename="data/osm_buildings_${project_id}.zip"
     while : ; do
-        if [ ! -f "data/osm_buildings_${project_id}.zip" ]; then
-            echo "Downloading data/osm_buildings_${project_id}.zip ... STATUS(${current_project} of ${num_project_ids})"
-            curl -s -X GET -d "${PAYLOAD}" "${API_ENDPOINT}hotosm_project_${project_id}/buildings/polygons/hotosm_project_${project_id}_buildings_polygons_geojson.zip" > "data/osm_buildings_${project_id}.zip"
+        if [ ! -f "${filename}" ]; then
+            echo "Downloading ${filename} ... STATUS(${current_project} of ${num_project_ids})"
+            curl -s -X GET -d "${PAYLOAD}" "${API_ENDPOINT}hotosm_project_${project_id}/buildings/polygons/hotosm_project_${project_id}_buildings_polygons_geojson.zip" > "${filename}"
             if [ $? -eq 0 ]; then
-                if ! file --mime-type "data/osm_buildings_${project_id}.zip" | grep -q "application/zip"; then
-                    if cat "data/osm_buildings_${project_id}.zip" | grep -q "NoSuchKey"; then
+                if ! file --mime-type "${filename}" | grep -q "application/zip"; then
+                    if cat "${filename}" | grep -q "NoSuchKey"; then
                         echo "Project ${project_id} has no buildings, deleting response."
-                        rm "data/osm_buildings_${project_id}.zip"
+                        rm "${filename}"
                     else
                         echo "Failed to download project ${project_id}."
                         exit 1
@@ -61,7 +88,7 @@ for project_id in $project_ids; do
             echo "Failed to download project ${project_id}. Retrying in 3 seconds..."
             sleep 3
         else
-            echo "File data/osm_buildings_${project_id}.zip already exists. Skipping download."
+            echo "File ${filename} already exists. Skipping download."
             break
         fi
     done
@@ -75,15 +102,16 @@ echo "************************"
 PAYLOAD="{}"
 current_project=1
 for project_id in $project_ids; do
+    filename="data/osm_roads_${project_id}.zip"
     while : ; do
-        if [ ! -f "data/osm_roads_${project_id}.zip" ]; then
-            echo "Downloading data/osm_roads_${project_id}.zip ... STATUS(${current_project} of ${num_project_ids})"
-            curl -s -X GET -d "${PAYLOAD}" "${API_ENDPOINT}hotosm_project_${project_id}/roads/lines/hotosm_project_${project_id}_roads_lines_geojson.zip" > "data/osm_roads_${project_id}.zip"
+        if [ ! -f "${filename}" ]; then
+            echo "Downloading ${filename} ... STATUS(${current_project} of ${num_project_ids})"
+            curl -s -X GET -d "${PAYLOAD}" "${API_ENDPOINT}hotosm_project_${project_id}/roads/lines/hotosm_project_${project_id}_roads_lines_geojson.zip" > "${filename}"
             if [ $? -eq 0 ]; then
-                if ! file --mime-type "data/osm_roads_${project_id}.zip" | grep -q "application/zip"; then
-                    if cat "data/osm_roads_${project_id}.zip" | grep -q "NoSuchKey"; then
+                if ! file --mime-type "${filename}" | grep -q "application/zip"; then
+                    if cat "${filename}" | grep -q "NoSuchKey"; then
                         echo "Project ${project_id} has no roads, deleting response."
-                        rm "data/osm_roads_${project_id}.zip"
+                        rm "${filename}"
                     else
                         echo "Failed to download project ${project_id}."
                         exit 1
@@ -94,7 +122,7 @@ for project_id in $project_ids; do
             echo "Failed to download project ${project_id}. Retrying in 3 seconds..."
             sleep 3
         else
-            echo "File data/osm_roads_${project_id}.zip already exists. Skipping download."
+            echo "File ${filename} already exists. Skipping download."
             break
         fi
     done
@@ -108,17 +136,18 @@ echo "***************************"
 PAYLOAD="{\"as_file\": false}"
 current_project=1
 for project_id in $project_ids; do
+    filename="data/grid_${project_id}.geojson"
     while : ; do
-        if [ ! -f "data/grid_${project_id}.geojson" ]; then
-            echo "Downloading data/grid_${project_id}.geojson ... STATUS(${current_project} of ${num_project_ids})"
-            curl -s -X GET -H "Accept-Language: en;accept: application/json" -d "${PAYLOAD}" "${API_ENDPOINT}projects/${project_id}/tasks/" > "data/grid_${project_id}.geojson"
+        if [ ! -f "${filename}" ]; then
+            echo "Downloading ${filename} ... STATUS(${current_project} of ${num_project_ids})"
+            curl -s -X GET -H "Accept-Language: en;accept: application/json" -d "${PAYLOAD}" "${API_ENDPOINT}projects/${project_id}/tasks/" > "${filename}"
             if [ $? -eq 0 ]; then
                 break
             fi
             echo "Failed to download project ${project_id}. Retrying in 3 seconds..."
             sleep 3
         else
-            echo "File data/grid_${project_id}.json already exists. Skipping download."
+            echo "File ${filename} already exists. Skipping download."
             break
         fi
     done
