c150f023f90daea5c5a1f081e379321b13ecb442
robot8a <cie.hochoa@gmail.com>
2025-02-26 17:48:43 +0100

    Add function to select random mockup grids and populate related tables

--- 08A_cont_dup_populate_mockup_grids.sql
+++ 08A_cont_dup_populate_mockup_grids.sql
@@ -1,5 +1,88 @@
+-- Select the mockup grids to cover the desired percentage
+DROP FUNCTION IF EXISTS select_random_mockup_grids;
+CREATE OR REPLACE FUNCTION select_random_mockup_grids(percentage_covered DOUBLE PRECISION, project_id INTEGER)
+RETURNS TABLE(taskid INTEGER, geom GEOMETRY) AS $$
+DECLARE
+	task RECORD;
+	mockup_polygon RECORD;
+    target_total_area DOUBLE PRECISION;
+    accumulated_area DOUBLE PRECISION := 0.0;
+    remaining_area DOUBLE PRECISION;
+	dilate_tolerance DOUBLE PRECISION;
+	dilate_tolerance_factor_of_area DOUBLE PRECISION := 20;
+	dilate_guess DOUBLE PRECISION;
+	dilate_guess_factor_of_area DOUBLE PRECISION := 50;
+	dilate_safety INTEGER := 1000000000;
+BEGIN
+    CALL raise_notice('Project ' || project_id || ' | Percentage ' || percentage_covered);
+
+    -- Select all distinct taskids for the project
+    FOR task IN SELECT * FROM grids g WHERE g.project_id = select_random_mockup_grids.project_id
+    LOOP
+        taskid := task.taskid;
+
+        -- Calculate the target total area
+        SELECT (ST_Area(task.geom_utm) / 100.0 * percentage_covered) INTO target_total_area;
+
+        -- Create a temporary table for the geometries
+        IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'temp_mockup_grids') THEN
+            DROP TABLE temp_mockup_grids;
+        END IF;
+        CREATE TEMP TABLE temp_mockup_grids AS
+        SELECT mg.geom, ST_Area(mg.geom) AS area
+        FROM mockup_grids mg
+        WHERE mg.project_id = select_random_mockup_grids.project_id AND mg.taskid = task.taskid;
+
+        accumulated_area := 0.0;
+        -- Iterate over the rows in the temporary table
+        FOR mockup_polygon IN
+            SELECT tmg.geom, tmg.area FROM temp_mockup_grids tmg
+            ORDER BY random() -- Shuffle the rows
+        LOOP
+            -- Add the area of the current geometry to the accumulated area
+            accumulated_area := accumulated_area + mockup_polygon.area;
+
+            -- If we went over the target total area, clip the geometry to cover the remaining area
+            IF accumulated_area > target_total_area THEN
+                -- Set tolerance and guess for ST_Dilate
+                dilate_tolerance := mockup_polygon.area / dilate_tolerance_factor_of_area;
+                dilate_guess := mockup_polygon.area / dilate_guess_factor_of_area;
+
+                -- Clip the geometry to cover the remaining area
+                remaining_area := target_total_area - (accumulated_area - mockup_polygon.area);
+                mockup_polygon.geom := ST_Dilate(
+                    mockup_polygon.geom,
+                    remaining_area / mockup_polygon.area,
+                    tol => dilate_tolerance,
+                    guess => dilate_guess,
+                    safety => dilate_safety
+                );
+
+                -- Handle ST_Dilate failure
+                IF mockup_polygon.geom IS NULL THEN
+                    RAISE NOTICE 'Failed to clip geometry, returning original geometry';
+                END IF;
+
+                -- Set accumulated area to target total area
+                accumulated_area := target_total_area;
+            END IF;
+
+            -- Return the selected geometry
+            geom := mockup_polygon.geom;
+            RETURN NEXT;
+
+            -- Stop if target area is covered
+            EXIT WHEN accumulated_area >= target_total_area;
+        END LOOP;
+    END LOOP;
+    DROP TABLE IF EXISTS temp_mockup_grids;
+    RETURN;
+END $$ LANGUAGE plpgsql;
+
 -- POPULATE MOCKUP POLYGON GRIDS
 DO $$
+DECLARE
+    grid_size_factor FLOAT := 25;
 BEGIN
     SET min_parallel_index_scan_size = 100;
     SET min_parallel_table_scan_size = 100;
@@ -12,7 +95,7 @@ BEGIN
 	CALL raise_notice('Selecting projects');
 	DROP TABLE IF EXISTS temp_project_ids;
 	CREATE TEMP TABLE temp_project_ids AS
-	SELECT proj_id, typename
+	SELECT DISTINCT(proj_id)
 	FROM selected_projects
 	WHERE indicator_cont_dup = 7
 	ORDER BY proj_id
@@ -22,18 +105,39 @@ BEGIN
 
     -- Populate mockup grids
     CALL raise_notice('Populating mockup grids');
+
     IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'mockup_grids') THEN
-        CREATE TABLE mockup_grids AS
-        SELECT g.project_id, g.taskid, ST_Intersection(g.geom_utm, (ST_SquareGrid(sqrt(ST_Area(g.geom_utm)) / 100, g.geom_utm)).geom) AS geom
-        FROM grids g
-        JOIN temp_project_ids tpi ON g.project_id = tpi.proj_id;
-    ELSE
-        INSERT INTO mockup_grids (project_id, taskid, geom)
-        SELECT g.project_id, g.taskid, ST_Intersection(g.geom_utm, (ST_SquareGrid(sqrt(ST_Area(g.geom_utm)) / 100, g.geom_utm)).geom) AS geom
-        FROM grids g
-        JOIN temp_project_ids tpi ON g.project_id = tpi.proj_id;
+        CREATE TABLE mockup_grids (project_id INTEGER, taskid INTEGER, geom GEOMETRY);
     END IF;
+
+    INSERT INTO mockup_grids (project_id, taskid, geom)
+    SELECT g.project_id, g.taskid, ST_Intersection(g.geom_utm, (ST_SquareGrid(sqrt(ST_Area(g.geom_utm)) / grid_size_factor, g.geom_utm)).geom) AS geom
+    FROM grids g
+    JOIN temp_project_ids tpi ON g.project_id = tpi.proj_id;
+
     CALL raise_notice('Mockup grids populated');
+
+    -- Create a table to store the selected mockup grids
+	CALL raise_notice('Selecting random mockup grids');
+
+	IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'mockup_selected_grids') THEN
+        CREATE TABLE mockup_selected_grids (proj_id INTEGER, taskid INTEGER, geom GEOMETRY, percentage_covered DOUBLE PRECISION);
+    END IF;
+
+    INSERT INTO mockup_selected_grids (proj_id, taskid, geom, percentage_covered)
+    SELECT tpi.proj_id, srmg.taskid, srmg.geom, 5.0 AS percentage_covered
+    FROM temp_project_ids tpi
+    CROSS JOIN LATERAL (SELECT * FROM select_random_mockup_grids(5.0, tpi.proj_id)) srmg
+    UNION ALL
+    SELECT tpi.proj_id, srmg.taskid, srmg.geom, 10.0 AS percentage_covered
+    FROM temp_project_ids tpi
+    CROSS JOIN LATERAL (SELECT * FROM select_random_mockup_grids(10.0, tpi.proj_id)) srmg
+    UNION ALL
+    SELECT tpi.proj_id, srmg.taskid, srmg.geom, 15.0 AS percentage_covered
+    FROM temp_project_ids tpi
+    CROSS JOIN LATERAL (SELECT * FROM select_random_mockup_grids(15.0, tpi.proj_id)) srmg;
+
+    CALL raise_notice('Random mockup grids selected');
 
     -- Update the selected projects to indicate that they have been processed
 	UPDATE selected_projects
